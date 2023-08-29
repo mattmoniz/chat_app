5e7bba1a1d7ccc7f4a75dd268b562f24f2951ab8
Matthew Moniz <Matt@Fastforward.sh>
2023-08-29 17:45:45 -0400

    fixed chatengine error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="8d977bd6-1509-43c9-ac23-31e35e5235d2"
+      userName="MMoniz543"
+      userSecret="test123"
+    />
   );
-}
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
