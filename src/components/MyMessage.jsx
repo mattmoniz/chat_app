9a20f6f19386763675e604c0c1703251d5e87721
Matthew Moniz <Matt@Fastforward.sh>
2023-09-05 21:35:32 -0400

    Finished theirmessage component

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const MyMessage = () => {
+const MyMessage = ({message}) => {
+    if (message?.attachments?.length > 0){
+        return (
+            <img src={message.attachments[0].file}
+                alt="message-attachment"
+                className='message-image'
+                styles={{float:'right'}}
+
+            />
+        )
+    }
   return (
-    <div>MyMessage</div>
+    <div className='message' style={{float:'right', marginRight:'18px', backgroundColor:'#3B2A50'}}>
+        {message.text}</div>
   )
 }
 
