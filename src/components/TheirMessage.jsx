0d6cb297d9ca2f99859d22fa34b26bf40d8b30cd
Matthew Moniz <Matt@Fastforward.sh>
2023-09-06 21:23:41 -0400

    updated chatfeed

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-
-const TheirMessage = ({lastMessage,message}) => {
-    const isFirstMessageByUser = !lastMessage || lastMessage.sender.userName !== message.sender.userName
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
 
   return (
     <div className="message-row">
       {isFirstMessageByUser && (
         <div
           className="message-avatar"
-          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+          style={{
+            backgroundImage: message.sender && `url(${message.sender.avatar})`,
+          }}
         />
       )}
-      ;
-      {message?.attachments?.length > 0 ? (
+      {message.attachments && message.attachments.length > 0 ? (
         <img
           src={message.attachments[0].file}
           alt="message-attachment"
           className="message-image"
-          styles={{ marginLeft: isFirstMessageByUser ? "4px" : "48px" }}
+          style={{ marginLeft: isFirstMessageByUser ? "4px" : "48px" }}
         />
       ) : (
         <div
@@ -33,6 +33,6 @@ const TheirMessage = ({lastMessage,message}) => {
       )}
     </div>
   );
-}
+};
 
-export default TheirMessage
+export default TheirMessage;
